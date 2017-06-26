ceaf8ba115f9890c3a50db4433be7343c4c76128
Taylor Sarrafian <surprisetalk@gmail.com>
2017-06-26 02:42:44 -0700

    more

--- src/App.elm
+++ src/App.elm
@@ -1,55 +1,154 @@
+
 module App exposing (..)
 
-import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
+
+-- IMPORTS ---------------------------------------------------------------------
+
+import Html exposing ( Html, Attribute, main_, text, div )
+import Html.Attributes as Attr exposing ( class )
+
+import Bulma.CDN              as CDN       exposing (..)
+
+-- import Bulma.Helpers          as Helps     exposing (..)
+
+import Bulma.Grid.Columns     as Columns   exposing (..)
+
+import Bulma.Layout.Hero      as Hero      exposing (..)
+-- import Bulma.Layout.Footer    as Footer    exposing (..)
+import Bulma.Layout.Section   as Section   exposing (..)
+import Bulma.Layout.Container as Container exposing (..)
+
+import Bulma.Elements.Image   as Image     exposing (..)
+import Bulma.Elements.Title   as Title     exposing (..)
+
+import List exposing ( map, concat )
 
 
----- MODEL ----
+-- HEPLERS ---------------------------------------------------------------------
+
+ls : a -> List a
+ls = flip (::) []
 
 
-type alias Model =
-    { message : String
-    , logo : String
-    }
+-- ALIASES ---------------------------------------------------------------------
+
+type alias Htmls msg = List (Html msg)
+
+type alias Attrs msg = List (Attribute msg)
+
+
+-- MODEL -----------------------------------------------------------------------
+
+type alias Model = {}
 
 
 init : String -> ( Model, Cmd Msg )
-init path =
-    ( { message = "Your Elm App is working!", logo = path }, Cmd.none )
+init path = {} ! []
 
 
+-- MSG -------------------------------------------------------------------------
 
----- UPDATE ----
+type Msg = NoOp
 
 
-type Msg
-    = NoOp
-
+-- UPDATE ----------------------------------------------------------------------
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+update msg model
+  = model ! []
 
 
+-- VIEW ------------------------------------------------------------------------
 
----- VIEW ----
-
+-- TODO: should be interactive! to show off our skillsssss
 
 view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src model.logo ] []
-        , div [] [ text model.message ]
-        ]
+view model
+  = main_ []
+  <| (::) CDN.stylesheet
+  <| concat
+    [ hello
+    , services
+    , team
+    , contact
+    , foot
+    ]
+
+hello : Htmls Msg
+hello
+  = easyTitle "SOPHWARE"
+    |> Title.size1
+    |> Title.toHtml
+    |> ls
+    |> container []
+    |> hero []
+    |> fullHeight
+    |> primary
+    |> Hero.toHtml
+    |> ls
+
+services : Htmls Msg
+services
+  = map Section.toHtml
+  <| map (section [])
+  <| [ frontEnd
+    , backEnd
+    ]
+
+frontEnd : Container Msg
+frontEnd
+  = let title_ : Htmls msg
+        title_ = Title.toHtmls
+                 -- TODO: icon
+               <| easyTitleWithSubtitle
+                 "Design"
+                 "We love clean interfaces."
+
+        beautiful : Html msg
+        beautiful = Columns.toHtml
+                  <| columns []
+                    [ column []
+                      [ easyImage [] "http://bulma.io/images/placeholders/256x256.png"
+                        |> Image.toHtml
+                      ]
+                      |> tabletSize4
+                      |> desktopSize4
+                    , column []
+                    <| map Title.toHtml
+                    <| map (Title.addClass "has-text-left")
+                      [ easyTitle    "Design"                    |> Title.size2
+                      , easySubtitle "We love clean interfaces." |> Title.size4
+                      ]
+                    ]
+
+    in container []
+     -- <| (++) title_
+     <| [ beautiful
+       -- , interactive
+       -- , lightweight
+       -- , portfolio
+       ]
+
+backEnd : Container Msg
+backEnd
+-- TODO: enterprise/fringe
+  = container [] []
+
+team : Htmls Msg
+team = []
+
+contact : Htmls Msg
+contact = []
+
+foot : Htmls Msg
+foot = []
 
 
-
----- PROGRAM ----
-
+-- MAIN ------------------------------------------------------------------------
 
 main : Program String Model Msg
-main =
-    Html.programWithFlags
+main
+  = Html.programWithFlags
         { view = view
         , init = init
         , update = update
