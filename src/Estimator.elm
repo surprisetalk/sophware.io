f75dc2144bd06190b4beb0ecfb2fc05c05f435cc
Taylor Sarrafian <surprisetalk@gmail.com>
2017-07-05 00:39:57 -0700

    more estimator stuff

--- src/Estimator.elm
+++ src/Estimator.elm
@@ -1,14 +1,51 @@
 
-module Estimator exposing (..)
+module Estimator exposing ( Estimate, init, view )
 
 -- TODO: at the end, we should have upfront & monthly totals (eg maintenance)
 
 -- TODO: don't be TOO granular
+-- TODO:   this is becoming a problem
 
 -- TODO: the dollar number should "count" up and "count" down animatedly
 -- TODO:   clicking should make like a video-game "ghost" effect, eg sims purchases?
 
 -- TODO: "if we didn't accurately capture everything you wanted, send us a message and we'll give you a better estimate"
+
+-- TODO: write sophware blog posts about these "new" markets we're trying to start
+
+-- TODO: progress bar indicating how much they've filled out
+-- TODO: or fixed bulma.menu on the left to tell you where you are + navigate
+
+-- TODO: are you sure you want to do this? you'll lose all progress
+
+-- TODO: at the end, give an easy/hard breakdown for all the options they picked, and realistic expectations
+
+-- TODO: "think your project is 'too hard'? we'll do a double-or-nothing wager!"
+
+{-
+
+|                                    |
+|        SOPHWARE ESTIMATOR          |
+|                                    |
+|        |    options     |          |
+|        |    options     |          |
+--------------------------------------
+| nav    |    options     | remarks  |
+| nav    |    options     |          |
+| nav    |    options     | remarks  |
+|        |    options     | remarks  |
+| price  |    options     |          |
+--------------------------------------
+|        |    options     | remarks  |
+|        |    options     |          |
+|        |                |          |
+|        |    overview    |          |
+|        |    total       |          |
+
+-- TODO: hover over things for more details?
+-- TODO:   but that's not mobile-friendly
+
+-}
 
 
 -- IMPORTS ---------------------------------------------------------------------
@@ -21,7 +58,10 @@ module Estimator exposing (..)
 
 type alias Estimate
   = { project     : Maybe Project
-    , urgency     : Maybe Scale
+    , impossible  : Maybe Bool
+      -- TODO: "impossible" discount? haha
+      -- TODO: this NEEDS to be near the top
+    , urgency     : Maybe Urgency
     , nonProfit   : Maybe Nod
     , paymentPlan : Maybe PaymentPlan
     }
@@ -29,6 +69,15 @@ type alias Estimate
 type Nod = No
          | Yes
          | Dunno
+
+type Urgency = Yesterday
+             | Today
+             | ThisWeek
+             | ThisMonth
+             | ThisQuarter
+             | ThisYear
+             | ThisDecade
+             | Never
 
 type Scale = None
            | Low
@@ -69,17 +118,21 @@ type alias App
 type Platform = Mobile Mobile
               | Web    Web
 
-type AppCategory = Blog_      AppFeatures Blog
-                 | Store_     AppFeatures Store
-                 | Portfolio_ AppFeatures Portfolio
-                 | Service_   AppFeatures Service
-                 | Advanced_  AdvancedApp Service
-                 | Other_     AppFetaures Blog
-                                          Store
-                                          Portfolio
-                                          Service
-                                          AdvancedApp
-                   -- TODO: each of these should have unique admin options
+type AppCategory
+-- TODO: each of these should have unique admin options
+-- TODO: we may want to mov AppFeatures down into each alias record
+  = Blog_      AppFeatures Blog
+  | Store_     AppFeatures Store
+  | Portfolio_ AppFeatures Portfolio
+  | Service_   AppFeatures Service
+  | Game_      AppFeatures
+               AdvancedApp Service
+  | Advanced_  AdvancedApp Service
+  | Other_     AppFetaures Blog
+                           Store
+                           Portfolio
+                           Service
+                           AdvancedApp
 
 type alias AppFeatures
   = { accounts     : Maybe AppAccounts
@@ -90,7 +143,6 @@ type alias AppFeatures
     , commerce     : Maybe AppCommerce
     , external     : Maybe AppExternal
     , security     : Maybe Scale
-    , advanced     : Maybe AppAdvanced
     , contact      : Maybe Nod
     , management   : Maybe Nod
     , maintenance  : Maybe Nod
@@ -139,6 +191,7 @@ type alias AppExternal
 
 type alias AppAdvanced
   -- TODO: break these up into advanced and service
+  -- TODO: give some of these a "fun" discount
   = { hardCamera      : Maybe Nod
     , imageProcessing : Maybe Nod
     , bigData         : Maybe Nod
@@ -146,6 +199,8 @@ type alias AppAdvanced
     , academic        : Maybe Nod
     , textAnalysis    : Maybe Nod
     , iot             : Maybe Nod
+    , parallelism     : Maybe Nod
+    , metaProgramming : Maybe Nod
     }
 
 
@@ -180,6 +235,7 @@ type alias Improvement
     , importance    : Maybe Scale
     , connectedness : Maybe Scale
     , language      : List  Lang
+    , openSource    : Maybe Nod
     }
 
 type ImprovementNeed
@@ -222,6 +278,7 @@ type Lang
   -- TODO:   these are actually really important; make sure to 
   -- TODO: should we include other languages, so that we know what we're getting into?
   -- TODO:   we could always just charge extra for "ugly" langs
+  -- TODO: add some of these to the main page
       
 
 -- RESEARCH --------------------------------------------------------------------
@@ -236,12 +293,12 @@ type Lang
 -- TODO: "i want a general analysis" vs "i want to solve a problem"
 
 type Research
-  = Analysis
+  = Analysis Study
   | Problem
     -- TODO: tell them more about our problem-solving and give them the contact form
 
 type alias Study
-  = { data  : Maybe Nod   -- do they already have the data?
+  = { data  : Maybe Nod   -- do they need to gather the data?
     , quant : Maybe Nod
     , study : List  StudyType
     , pres  : List  Presentation
@@ -252,7 +309,7 @@ type StudyType
   | StudyBusiness_   StudyBusiness
   | StudyData_       StudyData
   | StudyInnovation_ -- R&D
-  | StudyScience_    -- other sciences
+  | StudyScience_    -- other sciences (what about things like "education"?)
     -- TODO: this list needs improvement
     -- TODO: this "general" study ideas might give the wrong impression
     -- TODO:   we need some "oddly" specific examples to show what "good" studies look like
@@ -286,6 +343,7 @@ type StudyBusiness
   | StudyEthics
 
 type Presentation
+-- TODO: "default" is short writeup
 -- TODO: make it clear that we don't do fancy academic/enterprise "speak"
 -- TODO:   "we like simple words"
 -- TODO:   we will not "massage" our findings to your liking
@@ -299,9 +357,6 @@ type Presentation
   | Article
   | Other
 
-
--- AUTOMATION ------------------------------------------------------------------
-    
       
 -- INIT ------------------------------------------------------------------------
 
